ab2340fd53ea82df4af2c442b0e8640b8dfeb628
Bradley <liu770807152@hotmail.com>
2021-12-07 16:33:36 +1100

    add new apis for videos

--- app/controller/default/home.js
+++ app/controller/default/home.js
@@ -18,8 +18,8 @@ class HomeController extends Controller {
       'article.introduction as introduction, ' +
       'article.viewCount as viewCount, ' +
       'article.addTime as time, ' +
-      'type.typeName as typeName ' +
-      'FROM article LEFT JOIN type ON article.typeId = type.id';
+      'catalog.name as catalogName ' +
+      'FROM article LEFT JOIN catalog ON article.catalogId = catalog.id';
     const results = await this.app.mysql.query(sql);
     results.forEach((cur, index) => {
       cur.time = dayjs(cur.time).format('ddd, MMM D, YYYY');
@@ -33,14 +33,50 @@ class HomeController extends Controller {
       'article.title as title, ' +
       'article.viewCount as viewCount, ' +
       'article.addTime as time, ' +
-      'article.articleContent as content, ' +
-      'type.typeName as typeName ' +
-      'FROM article LEFT JOIN type ON article.typeId = type.id ' +
+      'article.content as content, ' +
+      'catalog.name as catalogName ' +
+      'FROM article LEFT JOIN catalog ON article.catalogId = catalog.id ' +
       `WHERE article.id = ${id}`;
     const results = await this.app.mysql.query(sql);
     results.forEach((cur, index) => {
       cur.time = dayjs(cur.time).format('ddd, MMM D, YYYY');
     });
+    this.ctx.body = { result: results };
+  }
+  async getVideoList() {
+    const sql =
+      'SELECT video.id as id, ' +
+      'video.title as title, ' +
+      'video.introduction as introduction, ' +
+      'video.viewCount as viewCount, ' +
+      'video.addTime as time, ' +
+      'catalog.name as catalogName ' +
+      'FROM video LEFT JOIN catalog ON video.catalogId = catalog.id';
+    const results = await this.app.mysql.query(sql);
+    results.forEach((cur, index) => {
+      cur.time = dayjs(cur.time).format('ddd, MMM D, YYYY');
+    });
+    this.ctx.body = { result: results };
+  }
+  async getVideoById() {
+    const id = this.ctx.params.id;
+    const sql =
+      'SELECT video.id as id, ' +
+      'video.title as title, ' +
+      'video.viewCount as viewCount, ' +
+      'video.addTime as time, ' +
+      'video.content as content, ' +
+      'catalog.name as catalogName ' +
+      'FROM video LEFT JOIN catalog ON video.catalogId = catalog.id ' +
+      `WHERE video.id = ${id}`;
+    const results = await this.app.mysql.query(sql);
+    results.forEach((cur, index) => {
+      cur.time = dayjs(cur.time).format('ddd, MMM D, YYYY');
+    });
+    this.ctx.body = { result: results };
+  }
+  async getCatalogList() {
+    const results = await this.app.mysql.select('catalog');
     this.ctx.body = { result: results };
   }
 }
--- app/router/default.js
+++ app/router/default.js
@@ -3,4 +3,7 @@ module.exports = (app) => {
   router.get('/api/test', controller.default.home.test);
   router.get('/api/articleList', controller.default.home.getArticleList);
   router.get('/api/article/:id', controller.default.home.getArticleById);
+  router.get('/api/videoList', controller.default.home.getVideoList);
+  router.get('/api/video/:id', controller.default.home.getVideoById);
+  router.get('/api/catalogList', controller.default.home.getCatalogList);
 };
