85f4555347a9a31f5c8be32faa136ef98338ee31
Bradley <liu770807152@hotmail.com>
2022-01-14 19:27:21 +1100

    delete all related to path `/video`

--- app/controller/default/home.js
+++ app/controller/default/home.js
@@ -8,7 +8,6 @@ class HomeController extends Controller {
   async test() {
     const { ctx } = this;
     const result = await this.app.mysql.get('article', {});
-    console.log(result);
     ctx.body = result;
   }
   async getArticleList() {
@@ -21,9 +20,10 @@ class HomeController extends Controller {
       'catalog.name as catalogName ' +
       'FROM article LEFT JOIN catalog ON article.catalogId = catalog.id';
     const results = await this.app.mysql.query(sql);
-    results.forEach((cur, index) => {
+    results.forEach((cur) => {
       cur.time = dayjs(cur.time).format('ddd, MMM D, YYYY');
     });
+    console.log('articleList');
     this.ctx.body = { result: results };
   }
   async getArticleById() {
@@ -38,46 +38,15 @@ class HomeController extends Controller {
       'FROM article LEFT JOIN catalog ON article.catalogId = catalog.id ' +
       `WHERE article.id = ${id}`;
     const results = await this.app.mysql.query(sql);
-    results.forEach((cur, index) => {
-      cur.time = dayjs(cur.time).format('ddd, MMM D, YYYY');
-    });
-    this.ctx.body = { result: results };
-  }
-  async getVideoList() {
-    const sql =
-      'SELECT video.id as id, ' +
-      'video.title as title, ' +
-      'video.introduction as introduction, ' +
-      'video.viewCount as viewCount, ' +
-      'video.addTime as time, ' +
-      'catalog.name as catalogName ' +
-      'FROM video LEFT JOIN catalog ON video.catalogId = catalog.id';
-    const results = await this.app.mysql.query(sql);
-    results.forEach((cur, index) => {
-      cur.time = dayjs(cur.time).format('ddd, MMM D, YYYY');
-    });
-    this.ctx.body = { result: results };
-  }
-  async getVideoById() {
-    const id = this.ctx.params.id;
-    const sql =
-      'SELECT video.id as id, ' +
-      'video.title as title, ' +
-      'video.viewCount as viewCount, ' +
-      'video.addTime as time, ' +
-      'video.content as content, ' +
-      'catalog.name as catalogName ' +
-      'FROM video LEFT JOIN catalog ON video.catalogId = catalog.id ' +
-      `WHERE video.id = ${id}`;
-    const results = await this.app.mysql.query(sql);
-    results.forEach((cur, index) => {
+    results.forEach((cur) => {
       cur.time = dayjs(cur.time).format('ddd, MMM D, YYYY');
     });
     this.ctx.body = { result: results };
   }
   async getCatalogList() {
     const results = await this.app.mysql.select('catalog');
-    this.ctx.body = { result: results };
+    this.ctx.body = [...results];
+    console.log(this.ctx.body);
   }
 }
 
