e1c89dcc967b446fc3d74806a7e6d3a8f9cf4773
Bradley <liu770807152@hotmail.com>
2022-01-20 02:18:45 +1100

    finish APIs for admin

--- app/controller/admin/main.js
+++ app/controller/admin/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const dayjs = require('dayjs');
 const Controller = require('egg').Controller;
 
 class MainController extends Controller {
@@ -19,22 +20,76 @@ class MainController extends Controller {
     }
   }
   async addArticle() {
-    let article = this.ctx.request.body;
-    const result = await this.app.mysql.insert('article', article);
-    const insertSuccess = result.affectedRows === 1;
-    const insertId = result.insertId;
-    this.ctx.body = {
-      succeeded: insertSuccess,
-      insertId
-    };
+    const article = this.ctx.request.body;
+    if (article) {
+      const result = await this.app.mysql.insert('article', article);
+      const insertSuccess = result.affectedRows === 1;
+      const insertId = result.insertId;
+      this.ctx.body = {
+        succeeded: insertSuccess,
+        insertId
+      };
+    } else {
+      this.ctx.body = {
+        succeeded: false
+      };
+    }
   }
-  async updateArticle() {
-    let article = this.ctx.request.body;
-    const result = await this.app.mysql.update('article', article);
-    const updateSuccess = result.affectedRows === 1;
-    this.ctx.body = {
-      succeeded: updateSuccess
-    };
+  async getArticleById() {
+    const { id } = this.ctx.params;
+    if (id) {
+      const query =
+        'SELECT article.id as id, ' +
+        'article.title as title, ' +
+        'article.introduction as introduction, ' +
+        'article.addTime as addTime, ' +
+        'article.content as content, ' +
+        'catalog.id as catalogId ' +
+        'FROM article LEFT JOIN catalog ON article.catalogId = catalog.id ' +
+        `WHERE article.id = ${id}`;
+      const res = await this.app.mysql.query(query);
+      this.ctx.body = { data: res };
+    } else {
+      this.ctx.body = { error: 'ID is required!' };
+    }
+  }
+  async updateArticleById() {
+    const article = this.ctx.request.body;
+    if (article) {
+      const result = await this.app.mysql.update('article', article);
+      const updateSuccess = result.affectedRows === 1;
+      this.ctx.body = {
+        succeeded: updateSuccess
+      };
+    } else {
+      this.ctx.body = {
+        succeeded: false
+      };
+    }
+  }
+  async deleteArticleById() {
+    const { id } = this.ctx.params;
+    if (id) {
+      const res = await this.app.mysql.delete('article', { id: id });
+      this.ctx.body = res;
+    } else {
+      this.ctx.body = { error: 'ID is required!' };
+    }
+  }
+  async getArticleList() {
+    let query =
+      'SELECT article.id as id,' +
+      'article.title as title,' +
+      'CONCAT(SUBSTRING(article.introduction,1,100), "...") as introduction, ' +
+      'article.addTime as time, ' +
+      'catalog.name as catalog ' +
+      'FROM article LEFT JOIN catalog ON article.catalogId = catalog.Id ' +
+      'ORDER BY article.id DESC ';
+    const list = await this.app.mysql.query(query);
+    list.forEach((cur) => {
+      cur.time = dayjs(cur.time).format('YYYY-MM-DD HH:mm:ss');
+    });
+    this.ctx.body = { list };
   }
 }
 
--- app/router/admin.js
+++ app/router/admin.js
@@ -2,11 +2,18 @@ module.exports = (app) => {
   const { router, controller } = app;
   const auth = app.middleware.auth();
   router.get('/admin/test', controller.admin.main.test);
-  router.post('/admin/checkLogin', auth, controller.admin.main.checkLogin);
-  router.post('/admin/addArticle', auth, controller.admin.main.addArticle);
+  router.post('/admin/checkLogin', controller.admin.main.checkLogin);
+  router.post('/admin/article', auth, controller.admin.main.addArticle);
+  router.get('/admin/article/:id', auth, controller.admin.main.getArticleById);
   router.post(
-    '/admin/updateArticle',
+    '/admin/article/:id',
     auth,
-    controller.admin.main.updateArticle
+    controller.admin.main.updateArticleById
   );
+  router.delete(
+    '/admin/article/:id',
+    auth,
+    controller.admin.main.deleteArticleById
+  );
+  router.get('/admin/articleList', auth, controller.admin.main.getArticleList);
 };
